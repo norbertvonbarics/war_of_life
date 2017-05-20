763c149ebb7fc15bff2642598323361dd7447bd2
gy0p4k <gyorgypakozdi@gmail.com>
2017-05-21 00:04:16 +0200

    cell model added

---
+++ src/model/Cell.java
@@ -0,0 +1,46 @@
+package model;
+
+/**
+ * Created by gy0p4k on 5/20/2017.
+ */
+public class Cell {
+  private int x;
+  private int y;
+  private boolean isAlive;
+
+  public Cell(int x, int y){
+    this.x = x;
+    this.y = y;
+    this.isAlive = false;
+  }
+
+  public Cell(int x, int y, boolean isAlive) {
+    this.x = x;
+    this.y = y;
+    this.isAlive = isAlive;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  public boolean isAlive() {
+    return isAlive;
+  }
+
+  public void setAlive(boolean alive) {
+    isAlive = alive;
+  }
+}
