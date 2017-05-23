c18e2a52d829d0e2688a92b862c4184debf4f0f7
gy0p4k <gyorgypakozdi@gmail.com>
2017-05-23 10:57:04 +0200

    added default gamemode (occupy) and working prototype of checker

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -12,34 +12,36 @@ import java.util.Collections;
 /**
  * Created by gy0p4k on 5/21/2017.
  */
-public class GameEngine  {
+public class GameEngine {
   ArrayList<GameObject> gameObjects;
   Map map;
   int x;
   int y;
   double prob;
-  ArrayList<Player> players= new ArrayList<>();
+  ArrayList<Player> players = new ArrayList<>();
   Board board;
   SafeZone safeZoneUp;
   SafeZone safeZoneDown;
+  String gametype;
+  Player p1;
+  Player p2;
 
-
-
-  public GameEngine(int x, int y,double prob) throws InterruptedException {
+  public GameEngine(int x, int y, double prob) throws InterruptedException {
     gameObjects = new ArrayList<>();
     this.x = x;
     this.y = y;
     this.prob = prob;
-    map = new Map(x,y, prob);
+    map = new Map(x, y, prob);
     gameObjects.addAll(map.getCells());
+    gametype = "occupy";
 
-    safeZoneUp = new SafeZone(12,26);
+    safeZoneDown = new SafeZone(12, 26);
+    safeZoneUp = new SafeZone(12, -2);
     gameObjects.addAll(safeZoneUp.getFields());
-    safeZoneDown = new SafeZone(12,-2);
     gameObjects.addAll(safeZoneDown.getFields());
 
-    Player p1 = new Player(12,26, gameObjects, "p1");
-    Player p2 = new Player(12,-2, gameObjects, "p2");
+    p1 = new Player(12, 26, gameObjects, "p1");
+    p2 = new Player(12, -2, gameObjects, "p2");
     players.add(p1);
     players.add(p2);
     gameObjects.addAll(players);
@@ -50,36 +52,62 @@ public class GameEngine  {
   }
 
 
-
   public void start() throws InterruptedException {
 
 
     //board.run();
-    while(true){
+    while (true) {
       map.nextLifeCycle();
       Thread.sleep(100);
       board.frame.repaint();
       checkDead();
-      Thread.sleep(100);
+      winchecker(gametype);
     }
   }
 
   public void checkDead() throws InterruptedException {
-    for(Player temp : players){
-        if(temp.isSamePlaceThanAnyCell()){
-          temp.setHp( temp.getHp() - 1);
-          playerReturner();
-          map.randomize();
-        }
+    for (Player temp : players) {
+      if (temp.isSamePlaceThanAnyCell()) {
+        temp.setHp(temp.getHp() - 1);
+        playerReturner();
+        map.randomize();
       }
+    }
 
   }
 
-  public void playerReturner(){
-    for(Player temp : players){
+  public void playerReturner() {
+    for (Player temp : players) {
 
-        temp.starterPosition();
+      temp.starterPosition();
 
+    }
+  }
+
+  public void winchecker(String gametype) {
+    switch (gametype) {
+
+      case "occupy":
+        occupyChecker(p1, safeZoneUp);
+        occupyChecker(p2, safeZoneDown);
+        break;
+
+      case "":
+        break;
+
+      default:
+        throw new IllegalArgumentException("Invalid gametype: " + gametype);
+    }
+  }
+
+
+  public void occupyChecker(Player p, SafeZone safeZone) {
+    for (GameObject temp : safeZone.getFields()) {
+      if (temp.isTheSamePlace(p)) {
+        p.setScore(p.getScore() + 1);
+        playerReturner();
+        map.randomize();
+      }
     }
   }
 
